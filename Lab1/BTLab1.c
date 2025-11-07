8bf16e9a12fd4a2a45f738a5bc4094fb5ea71302
lam quoc tu <lamquoctu2003@gmail.com>
2025-11-07 11:53:57 +0700

    Nop bai Lab1

---
+++ Lab1/BTLab1.c
@@ -0,0 +1,15 @@
+#incule <stdio.h>
+int main() {
+    // printf("Hello World\n");
+    // printf("Hello World\n");
+
+    int a,b;
+    char ten[10]; // cap nhat ki tu
+    printf("Nhap a va b");
+    scanf("%d %d", &a, &b);
+    printf("Nhap vao ten toi");
+    scanf("%s", &ten);;
+
+    printf("Xin chào tôi tên la: %s", ten);
+    return 0;
+}
